a82e0f8d319c8db821e3431a410579326f333ccb
nishantbuktare <nishant.buktare@gmail.com>
2023-10-17 15:33:34 +0530

    drupal

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
-cd sample/out
+
+# Define the Drupal CMS URL and authentication credentials
+DRUPAL_URL="http://localhost/drupal/"
+DRUPAL_USERNAME="root"
+DRUPAL_PASSWORD="*#Nishant1995"
+
+# Change to the directory where your WebHelp Responsive content is located
+cd sample/out/webhelp-responsive
+
+# Get the short hash of the latest commit
 rev=$(git rev-parse --short HEAD)
-git init
-git config user.name ${USER_NAME}
-git config user.email ${USER_MAIL}
-git remote add upstream ${GIT_REMOTE}
-git fetch upstream
-git reset upstream/gh-pages
-git add .
-git commit -m "Rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+
+# Example using 'curl' to create a new node in Drupal
+# Replace "your_content_type" with the machine name of your content type
+# You might need to customize the JSON data to match your Drupal content type fields
+curl -X POST -H "Content-Type: application/json" -d '{
+  "title": "New WebHelp Responsive Content",
+  "type": "cms_content",
+  "field_description": "This is a description for your content."
+}' -u "${DRUPAL_USERNAME}:${DRUPAL_PASSWORD}" "${DRUPAL_URL}/entity/node?_format=json"
+
+# Handle any additional data or formatting as needed.
+
+# Display a message indicating the content has been published to Drupal
+echo "WebHelp Responsive content published to Drupal at ${DRUPAL_URL}"
