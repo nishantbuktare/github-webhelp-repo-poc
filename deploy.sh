f51cc5af719577c60912dbdcadd6afd287d58833
nishantbuktare <nishant.buktare@gmail.com>
2023-10-17 18:43:42 +0530

    deploy222

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ GITHUB_REPO="https://github.com/nishantbuktare/space-pages.git"
 BRANCH="gh-pages"
 TEMP_DIR="/tmp/webhelp_tmp"
 
-# Clean up any previous deployments
 rm -rf "$TEMP_DIR"
 
 # Clone the GitHub repository
@@ -17,19 +16,13 @@ WEBHELP_DIR="$TEMP_DIR/webhelp-responsive"
 
 # Check if the repository was cloned successfully
 if [ -d "$WEBHELP_DIR" ]; then
-    # Clear Drupal cache (may vary depending on your Drupal version)
-    drush --root="$DRUPAL_ROOT" cr
+    # Clear Drupal cache
+    php "$DRUPAL_ROOT/web/core/scripts/drupal" cache:rebuild
 
     # Copy webhelp files to Drupal
-    rsync -av "$WEBHELP_DIR/" "$DRUPAL_ROOT/sites/default/files/webhelp/"
+    cp -r "$WEBHELP_DIR/" "$DRUPAL_ROOT/sites/default/files/webhelp/"
 
-    # Optionally, you may need to update the database or perform other tasks specific to your deployment
-    # drush --root="$DRUPAL_ROOT" updatedb
-
-    # Clear Drupal cache again
-    drush --root="$DRUPAL_ROOT" cr
-
-    echo "WebHelp content deployed successfully."
+    echo "WebHelp content deployed successfully to your local Drupal instance."
 else
     echo "Failed to clone the GitHub repository or webhelp content not found."
     exit 1
