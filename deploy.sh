35f9a2b2b362ac903132719f169d37ecd9a05950
nishantbuktare <nishant.buktare@gmail.com>
2023-10-17 16:17:51 +0530

    hhhhhhhhhhh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Define the Drupal CMS URL and authentication credentials
-DRUPAL_URL="http://localhost:80"
+DRUPAL_URL="http://localhost/drupal:12.34.56.78:80"
 DRUPAL_USERNAME="root"
 DRUPAL_PASSWORD="*#Nishant1995"
 
