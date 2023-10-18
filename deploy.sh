029070c1ed7c1fd529517e0f63d7e6d224d8b1f1
nishantbuktare <nishant.buktare@gmail.com>
2023-10-18 16:33:27 +0530

    harshit

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,12 @@
-#!/bin/bash
-
-# Variables
-DRUPAL_ROOT="C:\xampp\htdocs\drupal"
-GITHUB_REPO="https://github.com/nishantbuktare/space-pages.git"
-BRANCH="gh-pages"
-TEMP_DIR="/tmp/webhelp_tmp"
-
-rm -rf "$TEMP_DIR"
-
-# Clone the GitHub repository
-git clone --branch "$BRANCH" "$GITHUB_REPO" "$TEMP_DIR"
-
-# Assuming your webhelp responsive content is located in a specific subfolder (e.g., "webhelp")
-WEBHELP_DIR="$TEMP_DIR/webhelp-responsive"
-
-# Check if the repository was cloned successfully
-if [ -d "$WEBHELP_DIR" ]; then
-    # Clear Drupal cache
-    php "$DRUPAL_ROOT/web/core/scripts/drupal" cache:rebuild
-
-    # Copy webhelp files to Drupal
-    cp -r "$WEBHELP_DIR/" "$DRUPAL_ROOT/sites/default/files/webhelp/"
-
-    echo "WebHelp content deployed successfully to your local Drupal instance."
-else
-    echo "Failed to clone the GitHub repository or webhelp content not found."
-    exit 1
-fi
-
-# Clean up temporary directory
-rm -rf "$TEMP_DIR"
+#!/bin/sh
+cd sample/out
+rev=$(git rev-parse --short HEAD)
+git init
+git config user.name ${USER_NAME}
+git config user.email ${USER_MAIL}
+git remote add upstream ${GIT_REMOTE}
+git fetch upstream
+git reset upstream/gh-pages
+git add .
+git commit -m "Rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
